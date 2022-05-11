c07eacb2ba95eec0abce9923f61ffb1e6fdb3f8b
Danila <de.levin.orp@gmail.com>
2022-05-11 21:37:41 +0300

    First and second numbers

---
+++ Example1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int num = new Random().Next(10, 100);
+int a1 = num / 10;
+int a2 = num % 10;
+if (a1 > a2)
+{
+    WriteLine($"{num} -> {a1}");
+}
+else
+{
+    WriteLine($"{num} -> {a2}");
+}
