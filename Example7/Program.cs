b804361c6b6d40dbf02971278c8a929777b90c8a
Danila <de.levin.orp@gmail.com>
2022-05-13 01:26:02 +0300

    n1 and n2 sqrs

---
+++ Example7/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход два числа и проверяет,
+является ли одно число квадратом другого.*/
+
+using System;
+using static System.Console;
+
+WriteLine("Enter n1");
+int n1 = Convert.ToInt32(ReadLine());
+WriteLine("Enter n2");
+int n2 = Convert.ToInt32(ReadLine());
+WriteLine(n1*n1==n2?$"{n2} is a sqr {n1}":n2*n2==n1?$"{n1} is a sqr {n2}":"isn't sqrs");
