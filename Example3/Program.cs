45c5c62dc9250257a0c6fd0af5a3429d82ad72dc
Danila <de.levin.orp@gmail.com>
2022-05-13 00:06:14 +0300

    Whithout second number for math

---
+++ Example3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число
+//и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+using System;
+using static System.Console;
+
+Clear();
+
+int num = new Random().Next(100, 1000);
+WriteLine(num);
+int a1 = num / 100;
+int a2 = num % 10;
+int result = a1*10+a2;
+WriteLine(result);
