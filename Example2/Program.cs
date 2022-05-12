95a951559ceaa06f0ee84c0d91fcacbef60adafb
Danila <de.levin.orp@gmail.com>
2022-05-13 00:53:45 +0300

    First and second numbers for string with ternary operator

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -7,7 +7,7 @@ int num = new Random().Next(10, 100);
 string numS = num.ToString();
 int a1 = Convert.ToInt32(numS[0].ToString());
 int a2 = Convert.ToInt32(numS[1].ToString());
-if (a1 > a2)
+/*if (a1 > a2)
 {
     WriteLine($"{num} -> {a1}");
 }
@@ -15,3 +15,6 @@ else
 {
     WriteLine($"{num} -> {a2}");
 }
+*/
+string answer = (a1 > a2)?($"{num} -> {a1}"):($"{num} -> {a2}");
+WriteLine(answer);
