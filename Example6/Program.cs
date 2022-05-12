6e01e0a0adb74c14e9d33023e455a19cdf9ae9b5
Danila <de.levin.orp@gmail.com>
2022-05-13 01:17:02 +0300

    n%7==0&&n%23==0?

---
+++ Example6/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да*/
+
+using System;
+using static System.Console;
+
+WriteLine("Enter n");
+int n = Convert.ToInt32(ReadLine());
+WriteLine(n%7==0&&n%23==0?"да":"нет");
