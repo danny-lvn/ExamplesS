9be77d0877ea055677200149e9bb821eda403c6d
Danila <de.levin.orp@gmail.com>
2022-05-13 00:21:14 +0300

    n1 % n2 = ...

---
+++ Example5/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить,
+//является ли первое число кратным второму.
+//Если первое число не кратно числу второму, то программа выводит остаток от деления.
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+using System;
+using static System.Console;
+
+WriteLine("Enter n1");
+int n1 = Convert.ToInt32(ReadLine());
+WriteLine("Enter n2");
+int n2 = Convert.ToInt32(ReadLine());
+if ((n1 % n2) == 0) WriteLine("кратно");
+else WriteLine($"не кратно, остаток {n1 % n2}");
