21592353a5dce35645781e762bc9dea6524ce0fa
Danila <de.levin.orp@gmail.com>
2022-05-12 23:48:28 +0300

    First and second numbers for string

---
+++ Example2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int num = new Random().Next(10, 100);
+string numS = num.ToString();
+int a1 = Convert.ToInt32(numS[0].ToString());
+int a2 = Convert.ToInt32(numS[1].ToString());
+if (a1 > a2)
+{
+    WriteLine($"{num} -> {a1}");
+}
+else
+{
+    WriteLine($"{num} -> {a2}");
+}
