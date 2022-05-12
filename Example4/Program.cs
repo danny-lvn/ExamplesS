e45be7e454a3b0397ca418cfeb750b3dc0e40fa7
Danila <de.levin.orp@gmail.com>
2022-05-13 00:10:19 +0300

    Whithout second number for string

---
+++ Example4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число
+//и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+using System;
+using static System.Console;
+
+Clear();
+
+int num = new Random().Next(100, 1000);
+WriteLine(num);
+string numS = num.ToString();
+string resNum = numS[0].ToString() + numS[2].ToString();
+WriteLine(resNum);
