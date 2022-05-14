9bde10d602d3dd5a88697b3b235b932ca47f31cd
Danila <de.levin.orp@gmail.com>
2022-05-14 23:52:16 +0300

    squares from 1 to N

---
+++ Example11/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter N");
+int N = Convert.ToInt32(ReadLine());
+int i = 1;
+while (i <= N)
+{
+    Write($"{Math.Pow(i, 2)} ");
+    i++;
+}
