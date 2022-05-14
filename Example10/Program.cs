bd32b825ae32dfc43ec4eaab6b74e95a06519cb6
Danila <de.levin.orp@gmail.com>
2022-05-14 23:46:08 +0300

    distance between points 2D

---
+++ Example10/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter xA");
+int xA = Convert.ToInt32(ReadLine());
+WriteLine("Enter yA");
+int yA = Convert.ToInt32(ReadLine());
+WriteLine("Enter xB");
+int xB = Convert.ToInt32(ReadLine());
+WriteLine("Enter yB");
+int yB = Convert.ToInt32(ReadLine());
+
+double res = Math.Round(Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2)), 2);
+WriteLine($"The distance between points is {res}");
