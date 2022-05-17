9508022ff646a03d459a70ee5caa4c3facca5ae7
Danila <de.levin.orp@gmail.com>
2022-05-17 22:58:58 +0300

    array to console in string

---
+++ Example12/Program.cs
@@ -0,0 +1,15 @@
+﻿// Стандартный метод вывода массива C#
+
+using System;
+using static System.Console;
+
+Clear();
+
+int[] r = new int[5];
+string t = "123456789";
+for (int i = 0; i < 5; i++)
+{
+    r[i] = new Random().Next(10);
+}
+WriteLine(String.Join(" ", r));
+WriteLine(String.Join(" ", t.ToArray()));
