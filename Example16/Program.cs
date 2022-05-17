3e6024857dcc3250139ceb0fa40fdcb568045f30
Danila <de.levin.orp@gmail.com>
2022-05-18 01:10:21 +0300

    method to out array

---
+++ Example16/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая выводит массив из 8 элементов, заполненный нулями и единицами
+// в случайном порядке.
+// (Через метод)
+
+using System;
+using static System.Console;
+
+Clear();
+arr();
+
+void arr()
+{
+    int[] r = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        r[i] = new Random().Next(2);
+    }
+WriteLine($"[{String.Join(",", r)}]");
+}
