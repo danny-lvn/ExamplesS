ab21f479174be98b90937c98c7497074a27a7c2b
Danila <de.levin.orp@gmail.com>
2022-05-18 00:34:06 +0300

    method multiple from 1 to N

---
+++ Example15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимиает на вход число N, и выдает произведение чисел от 1 до N.
+// (В виде отдельного метода)
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter N");
+WriteLine(sum(Convert.ToInt32(ReadLine())));
+
+int sum (int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= number; i++) sum *= i;
+    return sum;
+}
