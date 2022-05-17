8e33a65034507797c3f33d3cda06a9bf544da461
Danila <de.levin.orp@gmail.com>
2022-05-18 00:32:06 +0300

    method sum from 1 to A

---
+++ Example13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принмиает на вход число A, и выдает сумму чисел от 1 до A.
+// (В виде отдельного метода)
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter A");
+WriteLine(sum(Convert.ToInt32(ReadLine())));
+
+int sum (int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++) sum += i;
+    return sum;
+}
