dff20352f61ddca24d9b36733ff6a5a4adaf5ac5
Danila <de.levin.orp@gmail.com>
2022-05-18 00:32:46 +0300

    method count of digit in number

---
+++ Example14/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число и выдает количество цифра в этом числе.
+// (Математически через метод)
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter A");
+WriteLine(sod(Convert.ToInt32(ReadLine())));
+
+int sod(int number)
+{
+    int n = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        n++;
+    }
+    return n;
+}
