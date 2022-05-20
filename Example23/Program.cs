961e6f321a084d1009179421175c9e5584f99317
Danila <de.levin.orp@gmail.com>
2022-05-21 02:16:01 +0300

    LINQ!!!

---
+++ Example23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Power of Linq library
+
+using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+
+int[] array = new int[20].Select(x => new Random().Next(0, 1000)).ToArray();
+int k = array.Where(x=>x>9&&x<100).Count();
+WriteLine(String.Join(", ", array));
+WriteLine(k);
