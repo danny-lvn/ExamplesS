8ecdb6d602a6b5b81f0f8fa361a6147406ad31e4
Danila <de.levin.orp@gmail.com>
2022-05-21 01:14:48 +0300

    first * last... in array

--- Example22/Program.cs
+++ Example22/Program.cs
@@ -5,3 +5,36 @@
 [1 2 3 4 5] -> 5 8 3
 [6 7 3 6] -> 36 21
 */
+
+using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+
+int[] array = new int[new Random().Next(1, 10)].Select(x => new Random().Next(0, 10)).ToArray();
+
+switch (array.Length % 2)
+{
+	case 0:
+	{
+		int[] array1 = new int[array.Length / 2];
+		for (int i = 0; i <= array.Length / 2 - 1; i++)
+		{
+			array1[i] = array[i] * array[array.Length - 1 - i];
+		}
+		WriteLine($"[{String.Join(", ", array)}] -> [{String.Join(", ", array1)}]");
+        break;
+	}
+	case 1:
+	{
+		int[] array1 = new int[array.Length / 2 + 1];
+		for (int i = 0; i <= array.Length / 2 - 1; i++)
+		{
+			array1[i] = array[i] * array[array.Length - 1 - i];
+		}
+		array1[array.Length / 2] = array[array.Length / 2];
+		WriteLine($"[{String.Join(", ", array)}] -> [{String.Join(", ", array1)}]");
+        break;
+	}
+}
