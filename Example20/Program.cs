322227d1981612f3578507c4ea5c6985ff7e1d3c
Danila <de.levin.orp@gmail.com>
2022-05-20 09:27:54 +0300

    library LINQ!!!

---
+++ Example20/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Библиотека Linq - для работы с массивами
+*/
+
+using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter array");
+//           считали     разд. через пробел без нул. элем.            обрат. к кажд. и вз. с парс. и дел. масс.
+//int[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+//int[] array1 = array.Select(x => x * -1).ToArray();
+//int size = int.Parse(ReadLine());
+int[] array2 = new int[int.Parse(ReadLine())].Select(x => new Random().Next(-9, 10)).ToArray();
+//WriteLine(String.Join(" ", array));
+//WriteLine(String.Join(" ", array1));
+WriteLine(String.Join(" ", array2));
