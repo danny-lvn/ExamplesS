63a4df2c859e451932cc8f1ad8071864262b3482
Danila <de.levin.orp@gmail.com>
2022-05-23 00:27:40 +0300

    triangle or not?

--- Example25/Program.cs
+++ Example25/Program.cs
@@ -1,6 +1,15 @@
 ﻿/*
 **Задача 40:** Напишите программу, которая принимает на вход три числа и проверяет,
 может ли существовать треугольник с сторонами такой длины.
-❗ **Теорема о неравенстве треугольника:** каждая сторона треугольника меньше суммы двух других сторон.
+**Теорема о неравенстве треугольника:** каждая сторона треугольника меньше суммы двух других сторон.
 */
 
+using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter three numbers in format: a b c");
+int[] ar = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+WriteLine(ar[0] < ar[1] + ar[2] && ar[1] < ar[0] + ar[2] && ar[2] < ar[0] + ar[1] ? "Yes" : "No");
