a91b42fa98bc8699dab3fd9d7b46c4b65fd4999d
Danila <de.levin.orp@gmail.com>
2022-05-24 21:52:18 +0300

    Fibonacci row

--- Example27/Program.cs
+++ Example27/Program.cs
@@ -5,3 +5,22 @@
 Если N = 3 -> 0 1 1
 Если N = 7 -> 0 1 1 2 3 5 8
 */
+
+using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter a count of numbers in a row Fibonacci");
+int N = Convert.ToInt32(ReadLine());
+WriteLine($"{N} -> {String.Join(" ", FibMeth(N))}");
+
+int[] FibMeth(int num)
+{
+    int[] FibRow = new int[num];
+    FibRow[0] = 0;
+    FibRow[1] = 1;
+    for (int i = 2; i < num; i++) FibRow[i] = FibRow[i - 2] + FibRow[i - 1];
+    return FibRow;
+}
