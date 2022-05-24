b2bf13564be81e00c7af4ab6757cf06858d9fc23
Danila <de.levin.orp@gmail.com>
2022-05-24 21:41:41 +0300

    copy array

--- Example28/Program.cs
+++ Example28/Program.cs
@@ -2,3 +2,23 @@
 Задача 45: Напишите программу, которая будет создавать копию
 заданного массива с помощью поэлементного копирования.
 */
+
+using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+
+int[] Array = new int[new Random().Next(1, 11)].Select(x => x = new Random().Next(-99, 100)).ToArray();
+WriteLine(String.Join(" ", Array));
+WriteLine((String.Join(" ", copyArray(Array))));
+
+int[] copyArray(int[] array)
+    {
+    int[] Array1 = new int[Array.Length];
+    for (int i = 0; i < Array.Length; i++)
+        {
+            Array1[i] = Array[i];
+        }
+        return Array1;
+    }
