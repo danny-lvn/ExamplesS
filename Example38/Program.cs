1f7050f625ef345e1c1d2adceded35a5ec60b4f6
Danila <de.levin.orp@gmail.com>
2022-06-02 22:50:16 +0300

    sum of digit in number (recursion)

---
+++ Example38/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+**Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter a number");
+int Number = Convert.ToInt32(ReadLine());
+WriteLine(GetSumOfDigit(Number));
+
+int GetSumOfDigit(int num)
+{
+    if (num == 0) return num;
+    return (num % 10 + GetSumOfDigit(num / 10));
+}
