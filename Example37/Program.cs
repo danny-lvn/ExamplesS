722245c7997b1bef1b92887bf7a4e0e8f04563e6
Danila <de.levin.orp@gmail.com>
2022-06-02 16:41:00 +0300

    numbers from M to N (recursion)

---
+++ Example37/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+**Задача 65:** Задайте значения M и N. Напишите программу,
+которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter numbers M and N");
+int[] MatrixSize = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+string result = GetNumbers(MatrixSize[0], MatrixSize[1]);
+WriteLine(result);
+
+static string GetNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + " " + GetNumbers(start + 1, end));
+}
