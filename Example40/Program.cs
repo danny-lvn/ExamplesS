1073a199af2d774a9e5c9d8aa4afcb2402c44635
Danila <de.levin.orp@gmail.com>
2022-06-07 01:36:36 +0300

    Furie

--- Example40/Program.cs
+++ Example40/Program.cs
@@ -2,3 +2,19 @@
 На вход подаются две строки. Написать программу, которая определит,
 можно ли из верхней строки составить нижнюю.
 */
+
+using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter the first string");
+string Line1 = ReadLine();
+WriteLine("Enter the second string");
+string Line2 = ReadLine();
+
+string Line11 = String.Join("", Line1.ToLower().Replace(" ", "").OrderBy(c => c).ToArray());
+string Line22 = String.Join("", Line2.ToLower().Replace(" ", "").OrderBy(c => c).ToArray());
+
+WriteLine(Line11.Equals(Line22) ? "Yes" : "No");
---
+++ Example41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+**Задача 70:** Напишите программу, которая на вход принимает два числа и выдаёт первые N чисел,
+для которых каждое следующее равно сумме двух предыдущих.
+3 и 4, N = 5 -> 3 4 7 11 18
+6 и 10, N = 4 -> 6 10 16 26
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter two first numbers and count of next numbers");
+int[] Numbers = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+Write($"{Numbers[0]} {Numbers[1]} ");
+WriteLine(Furie(Numbers[0], Numbers[1], Numbers[2] - 2));
+
+string Furie(int start, int end, int length)
+{
+    return length == 0 ? " ": start + end + " " + Furie(end, start + end, length - 1);
+}
